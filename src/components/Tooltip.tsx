7385b26f557650211a445ef536cd655e350a9026
FB F3X <fabiojunior321@yahoo.com.br>
2024-03-22 14:24:08 -0300

    Text Tooltip Color

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -2,10 +2,12 @@ function Tooltip({
   children,
   message,
   color,
+  text,
 }: {
   children: React.ReactNode;
   message: string;
   color?: string;
+  text?: string;
 }) {
   return (
     <div className="group relative flex cursor-pointer">
@@ -14,7 +16,7 @@ function Tooltip({
         className={`absolute ${
           message.length > 0 ? "scale-100" : "scale-0"
         } -top-10 right-0  transition-all rounded  p-1 text-xs text-white text-center font-medium`}
-        style={{ backgroundColor: color || "#1ED760" }}
+        style={{ backgroundColor: color || "#1ED760", color: text || "#fff" }}
       >
         {message}
       </span>
