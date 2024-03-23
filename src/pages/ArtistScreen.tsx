ac85cd2218763119b9c92b7ad11721ecbc20b5ea
FB F3X <fabiojunior321@yahoo.com.br>
2024-03-23 12:59:42 -0300

    fix: Recommendation in Artist Screen and Follow in Discover

--- src/pages/ArtistScreen.tsx
+++ src/pages/ArtistScreen.tsx
@@ -1,29 +1,28 @@
-import { useParams } from "react-router-dom";
-import { ring2 } from "ldrs";
-import Carousel from "../components/Carousel";
-import { useAuth } from "../contexts/AuthContext";
-import Login from "../components/Login";
-import { useFetchRecommendations } from "../hooks/track/useFetchRecommendations";
-import { useFetchArtistTopTracks } from "../hooks/track/useFetchArtistTopTracks";
-import { useFetchArtist } from "../hooks/artist/useFetchArtist";
-import ShowArtist from "../components/ShowArtist";
+import { useParams } from 'react-router-dom';
+import { ring2 } from 'ldrs';
+import Carousel from '../components/Carousel';
+import { useAuth } from '../contexts/AuthContext';
+import Login from '../components/Login';
+import { useFetchRecommendations } from '../hooks/track/useFetchRecommendations';
+import { useFetchArtistTopTracks } from '../hooks/track/useFetchArtistTopTracks';
+import { useFetchArtist } from '../hooks/artist/useFetchArtist';
+import ShowArtist from '../components/ShowArtist';
 
 function ArtistScreen() {
   const { id } = useParams();
 
   const authContext = useAuth();
 
-  const { data: infos } = useFetchArtist(id || "");
+  const { data: infos } = useFetchArtist(id || '');
+
+  const { data: artistTracks } = useFetchArtistTopTracks(infos?.id || '', 10);
 
   const { data: reccomendations } = useFetchRecommendations(
     null,
-    null,
     infos?.id ? [infos.id] : null,
+    artistTracks?.slice(0, 4).map(a => a.id) || null,
     10
   );
-
-  const { data: artistTracks } = useFetchArtistTopTracks(infos?.id || "", 10);
-
   ring2.register();
 
   if (!authContext.accessToken) {
@@ -37,10 +36,7 @@ function ArtistScreen() {
         <div className="space-y-4">
           <h2 className="font-semibold"></h2>
           <Carousel infos={reccomendations} title="Reccomended Tracks" />
-          <Carousel
-            infos={artistTracks}
-            title={`${infos ? infos?.name : "Artist"} Top Tracks`}
-          />
+          <Carousel infos={artistTracks} title={`${infos ? infos?.name : 'Artist'} Top Tracks`} />
         </div>
       </div>
     </main>
--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1,23 +1,24 @@
-import React from "react";
-import Track from "../components/Track";
-import Login from "../components/Login";
-import { useAuth } from "../contexts/AuthContext";
-import { useFetchTopArtists } from "../hooks/artist/useFetchTopArtists";
-import { getTopGenres } from "../utils/utils";
-import { useFetchRecommendations } from "../hooks/track/useFetchRecommendations";
+import React from 'react';
+import Track from '../components/Track';
+import Login from '../components/Login';
+import { useAuth } from '../contexts/AuthContext';
+import { useFetchTopArtists } from '../hooks/artist/useFetchTopArtists';
+import { getTopGenres } from '../utils/utils';
+import { useFetchRecommendations } from '../hooks/track/useFetchRecommendations';
+import { useFetchFollowTracks } from '../hooks/track/useFetchFollowTracks';
 
 function Discover() {
   const authContext = useAuth();
 
-  const { data: topArtists } = useFetchTopArtists("long_term");
+  const { data: topArtists } = useFetchTopArtists('long_term');
 
   const seed_genres = topArtists && getTopGenres(topArtists, 5);
 
-  const { data: recommendations } = useFetchRecommendations(
-    seed_genres || null,
-    null,
-    null
-  );
+  const { data: recommendations } = useFetchRecommendations(seed_genres || null, null, null);
+
+  const validTracksIds = recommendations?.filter(track => track && track.id).map(track => track.id);
+
+  const { data: followData } = useFetchFollowTracks(validTracksIds || []);
 
   if (!authContext.accessToken) {
     return <Login />;
@@ -28,13 +29,17 @@ function Discover() {
       <div className="space-y-4">
         <span className="flex items-center justify-between">
           <span>
-            <h2 className="font-semibold text-lg">
-              Reccomended tracks for you
-            </h2>
+            <h2 className="font-semibold text-lg">Reccomended tracks for you</h2>
             <p className="text-zinc-300 text-sm">Based on your top genres</p>
           </span>
         </span>
         {recommendations?.map((item, index) => {
+          if (item) {
+            item = {
+              ...item,
+              followed: followData?.[index] ?? false,
+            };
+          }
           return (
             <React.Fragment key={index}>
               <Track infos={item} index={index + 1} />
