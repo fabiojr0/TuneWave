d2c250f8a203d67924516ae38464fb052c31b388
fabiojr0 <fabiojunior321@yahoo.com.br>
2024-03-24 00:07:14 -0300

    fix: Wrong importing

--- src/components/ShowArtist.tsx
+++ src/components/ShowArtist.tsx
@@ -1,16 +1,16 @@
-import { HeartStraight } from "@phosphor-icons/react";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
-import { useMutateFollowArtist } from "../hooks/artist/useMutateFollowArtist";
-import { useEffect, useState } from "react";
-import Tooltip from "./Tooltip";
-import { useFetchFollowArtist } from "../hooks/artist/useFetchFollowArtists";
+import { HeartStraight } from '@phosphor-icons/react';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import { useMutateFollowArtist } from '../hooks/artist/useMutateFollowArtist';
+import { useEffect, useState } from 'react';
+import Tooltip from './Tooltip';
+import { useFetchFollowArtists } from '../hooks/artist/useFetchFollowArtists';
 
 function ShowArtist({ infos }: { infos?: Artist }) {
-  const [showTooltip, setShowTooltip] = useState<TooltipProps>({ message: "" });
+  const [showTooltip, setShowTooltip] = useState<TooltipProps>({ message: '' });
 
   const [follow, setFollow] = useState<boolean>(infos?.followed ? true : false);
 
-  const { data: followData } = useFetchFollowArtist([infos?.id || ""]);
+  const { data: followData } = useFetchFollowArtists([infos?.id || '']);
 
   useEffect(() => {
     if (followData) {
@@ -24,28 +24,23 @@ function ShowArtist({ infos }: { infos?: Artist }) {
     mutateFollow({ artist_id, follow });
 
     setShowTooltip({
-      message: !follow ? "Followed" : "Unfollowed",
-      color: !follow ? "" : "darkgreen",
+      message: !follow ? 'Followed' : 'Unfollowed',
+      color: !follow ? '' : 'darkgreen',
     });
 
     setFollow(!follow);
 
     setTimeout(() => {
-      setShowTooltip({ message: "" });
+      setShowTooltip({ message: '' });
     }, 2000);
   };
 
   if (!infos) {
     return (
       <section>
-        <SkeletonTheme
-          baseColor="#585555"
-          highlightColor="#444"
-          width={"100%"}
-          height={"100%"}
-        >
+        <SkeletonTheme baseColor="#585555" highlightColor="#444" width={'100%'} height={'100%'}>
           <div className="space-y-4">
-            <Skeleton width={"100%"} className="aspect-square" />
+            <Skeleton width={'100%'} className="aspect-square" />
             <Skeleton />
             <Skeleton />
             <Skeleton />
@@ -73,17 +68,13 @@ function ShowArtist({ infos }: { infos?: Artist }) {
             <p className="text-zinc-300 text-sm font-medium">
               {infos?.genres
                 .slice(0, 3)
-                .map((genre) => genre)
-                .join(", ")}
+                .map(genre => genre)
+                .join(', ')}
             </p>
           )}
         </span>
         <span className="space-y-2">
-          <a
-            href={infos.external_urls.spotify}
-            target="_blank"
-            className="hover:scale-110 transition-all"
-          >
+          <a href={infos.external_urls.spotify} target="_blank" className="hover:scale-110 transition-all">
             <img
               src="../../Spotify_Icon_RGB_Green.png"
               alt="Open in Spotify"
@@ -93,7 +84,7 @@ function ShowArtist({ infos }: { infos?: Artist }) {
           <Tooltip message={showTooltip.message} color={showTooltip?.color}>
             <HeartStraight
               size={24}
-              weight={follow ? "fill" : "regular"}
+              weight={follow ? 'fill' : 'regular'}
               color="#1ED760"
               onClick={() => handleFollow(infos.id)}
             />
