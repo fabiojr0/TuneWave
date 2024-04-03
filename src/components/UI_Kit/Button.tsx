83dcdb2b8ea3db0b1e589318c6a896b9e421fd90
fabiojr0 <fabiojunior321@yahoo.com.br>
2024-04-03 18:39:42 -0300

    TwMerge And TailwindVariants for button

--- src/components/UI_Kit/Button.tsx
+++ src/components/UI_Kit/Button.tsx
@@ -1,33 +1,35 @@
 import { ring2 } from 'ldrs';
 import ActivityIndicator from './ActivityIndicator';
+import { ComponentProps } from 'react';
+import { tv, VariantProps } from 'tailwind-variants';
+import { twMerge } from 'tailwind-merge';
 
-function Button({
-  onClick,
-  children,
-  type = 'primary',
-  loading,
-  selected,
-}: {
-  onClick?: () => void;
-  children: React.ReactNode;
-  type?: 'primary' | 'time_range';
-  loading?: boolean;
-  selected?: boolean;
-}) {
-  const handleClick = () => {
-    if (onClick) {
-      onClick();
-    }
+const buttonStyles = tv({
+  base: 'font-semibold hover:scale-110 transition-all flex items-center justify-center gap-2 min-w-20 min-h-9 whitespace-nowrap',
+  variants: {
+    color: {
+      green: 'bg-darkGreen text-white',
+      black: 'bg-zinc-800 hover:text-lightGreen',
+    },
+    format: {
+      roundedFull: 'rounded-full p-2',
+      rounded: 'rounded-lg py-2 px-4 text-sm',
+    },
+  },
+  defaultVariants: {
+    color: 'green',
+    format: 'roundedFull',
+  },
+});
+
+type ButtonProps = ComponentProps<'button'> &
+  VariantProps<typeof buttonStyles> & {
+    buttonType?: 'primary' | 'time_range';
+    loading?: boolean;
+    selected?: boolean;
   };
 
-  const variants = {
-    primary:
-      'bg-darkGreen rounded-full p-2 flex items-center justify-center gap-2 text-white font-semibold hover:scale-110 transition-all min-w-20 min-h-9 ',
-    time_range: `bg-zinc-800 rounded-lg py-2 px-4 flex items-center justify-center gap-2 font-semibold hover:scale-110 transition-all text-sm min-w-20 min-h-9 whitespace-nowrap hover:text-lightGreen ${
-      selected ? 'text-darkGreen' : 'text-white'
-    }`,
-  };
-
+function Button({ buttonType = 'primary', loading, selected, children, ...props }: ButtonProps) {
   const loadingVariants = {
     primary: '#ffffff',
     time_range: '#1ED760',
@@ -35,9 +37,17 @@ function Button({
 
   ring2.register();
 
+  const buttonClass = twMerge(
+    buttonStyles({
+      color: buttonType === 'time_range' ? 'black' : 'green',
+      format: buttonType === 'primary' ? 'roundedFull' : 'rounded',
+    }),
+    selected && 'text-darkGreen'
+  );
+
   return (
-    <button onClick={handleClick} className={variants[type]}>
-      {loading ? <ActivityIndicator color={loadingVariants[type]} /> : children}
+    <button {...props} className={buttonClass}>
+      {loading ? <ActivityIndicator color={loadingVariants[buttonType || 'primary']} /> : children}
     </button>
   );
 }
--- src/components/UI_Kit/TimeRangeButtons.tsx
+++ src/components/UI_Kit/TimeRangeButtons.tsx
@@ -12,7 +12,7 @@ function TimeRangeButtons({
   return (
     <div className="flex items-center justify-center gap-4">
       <Button
-        type="time_range"
+        buttonType="time_range"
         onClick={() => chageTimeRange('short_term')}
         loading={time_range === 'short_term' && isLoading}
         selected={time_range === 'short_term'}
@@ -20,7 +20,7 @@ function TimeRangeButtons({
         4 Weeks ago
       </Button>
       <Button
-        type="time_range"
+        buttonType="time_range"
         onClick={() => chageTimeRange('medium_term')}
         loading={time_range === 'medium_term' && isLoading}
         selected={time_range === 'medium_term'}
@@ -28,7 +28,7 @@ function TimeRangeButtons({
         6 Months ago
       </Button>
       <Button
-        type="time_range"
+        buttonType="time_range"
         onClick={() => chageTimeRange('long_term')}
         loading={time_range === 'long_term' && isLoading}
         selected={time_range === 'long_term'}
