2d85958b90d2c3ce777c5e31f14a6822ec4127da
fabiojr0 <fabiojunior321@yahoo.com.br>
2024-04-03 18:54:40 -0300

    Close Modal on Outside click

--- src/components/UI_Kit/CreatePlaylistModal.tsx
+++ src/components/UI_Kit/CreatePlaylistModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useMutateCreatePlaylist } from '../../hooks/playlist/useMutateCreatePlaylist';
 import { useUserData } from '../../hooks/useUserData';
 import Button from './Button';
@@ -24,6 +24,8 @@ function CreatePlaylistModal({
 }) {
   const { data: user } = useUserData();
   const { mutate } = useMutateCreatePlaylist();
+
+  const modalRef = useRef<HTMLDivElement>(null);
 
   const navigate = useNavigate();
 
@@ -51,9 +53,15 @@ function CreatePlaylistModal({
     );
   };
 
+  const verifyClick = (target: EventTarget) => {
+    if (modalRef.current && !modalRef.current.contains(target as Node)) {
+      closeModal();
+    }
+  };
+
   const closeModal = () => {
-    setIsVisible(false); // Desativar a visibilidade do modal antes de sair da tela
-    setTimeout(() => setShowModal(false), 300); // Definir um atraso para fechar o modal após a animação
+    setIsVisible(false);
+    setTimeout(() => setShowModal(false), 300);
   };
 
   if (!user) {
@@ -61,63 +69,59 @@ function CreatePlaylistModal({
   }
 
   return (
-    <div>
-      <div className="fixed w-screen h-screen flex justify-center items-center z-50 top-0 left-0 p-4 bg-black/50" />
-      <div
-        className={`fixed w-screen h-screen flex justify-center items-center z-50 top-0 left-0 p-4 bg-black/50 transform transition-transform ${isVisible ? 'translate-y-0' : 'translate-y-full'}`}
-      >
-        <div className="bg-black p-4 rounded-lg space-y-4 border-2 border-darkGreen w-full max-w-96">
-          <div className="flex items-start">
-            <div>
-              <h1 className="text-2xl font-bold">Create Playlist</h1>
-              <p className="text-gray-400">Create a new playlist based on your top tracks</p>
-            </div>
-            <button onClick={closeModal}>
-              <X size={24} weight="regular" />
-            </button>
+    <div
+      className={`fixed w-screen h-screen flex justify-center items-center z-50 top-0 left-0 p-4 bg-black/50 transform transition-transform ${isVisible ? 'translate-y-0' : 'translate-y-full'}`}
+      onClick={e => verifyClick(e.target)}
+    >
+      <div className="bg-black p-4 rounded-lg space-y-4 border-2 border-darkGreen w-full max-w-96" ref={modalRef}>
+        <div className="flex items-start">
+          <div>
+            <h1 className="text-2xl font-bold">Create Playlist</h1>
+            <p className="text-gray-400">Create a new playlist based on your top tracks</p>
           </div>
-          <label className="flex flex-col gap-2 font-semibold" htmlFor="name">
-            Name
-            <input
-              type="text"
-              placeholder="Name"
-              value={playlistInfo.name}
-              onChange={e => setPlaylistInfo({ ...playlistInfo, name: e.target.value })}
-              className="bg-blackfy border-2 border-zinc-400 rounded-lg text-white px-4 py-2 hover:border-white cursor-pointer transition"
-              id="name"
-            />
-          </label>
-
-          <label className="flex flex-col gap-2 font-semibold" htmlFor="description">
-            Description
-            <textarea
-              placeholder="Description"
-              value={playlistInfo.description}
-              onChange={e => setPlaylistInfo({ ...playlistInfo, description: e.target.value })}
-              className="bg-blackfy border-2 border-zinc-400 rounded-lg text-white px-4 py-2 hover:border-white cursor-pointer transition"
-              id="description"
-            />
-          </label>
-
-          <label className="flex items-center gap-2 cursor-pointer">
-            <input
-              type="checkbox"
-              checked={playlistInfo.public}
-              onChange={e => setPlaylistInfo({ ...playlistInfo, public: e.target.checked })}
-              className="sr-only"
-            />
-            <div
-              className={`w-6 h-6 flex justify-center items-center rounded ${playlistInfo.public ? 'bg-lightGreen' : 'bg-gray-100'} border-2 border-gray-300`}
-            >
-              {playlistInfo.public && <Check size={24} color="white" />}{' '}
-            </div>
-            Public
-          </label>
-
-          <Button type="primary" onClick={handleCreatePlaylist}>
-            Salvar
-          </Button>
+          <button onClick={closeModal}>
+            <X size={24} weight="regular" />
+          </button>
         </div>
+        <label className="flex flex-col gap-2 font-semibold cursor-pointer" htmlFor="name">
+          Name
+          <input
+            type="text"
+            placeholder="Name"
+            value={playlistInfo.name}
+            onChange={e => setPlaylistInfo({ ...playlistInfo, name: e.target.value })}
+            className="bg-blackfy border-2 border-zinc-400 rounded-lg text-white px-4 py-2 hover:border-white cursor-text transition"
+            id="name"
+          />
+        </label>
+
+        <label className="flex flex-col gap-2 font-semibold cursor-pointer" htmlFor="description">
+          Description
+          <textarea
+            placeholder="Description"
+            value={playlistInfo.description}
+            onChange={e => setPlaylistInfo({ ...playlistInfo, description: e.target.value })}
+            className="bg-blackfy border-2 border-zinc-400 rounded-lg text-white px-4 py-2 hover:border-white cursor-text transition"
+            id="description"
+          />
+        </label>
+
+        <label className="flex items-center gap-2 cursor-pointer">
+          <input
+            type="checkbox"
+            checked={playlistInfo.public}
+            onChange={e => setPlaylistInfo({ ...playlistInfo, public: e.target.checked })}
+            className="sr-only"
+          />
+          <div
+            className={`w-6 h-6 flex justify-center items-center rounded ${playlistInfo.public ? 'bg-lightGreen' : 'bg-gray-100'} border-2 border-gray-300`}
+          >
+            {playlistInfo.public && <Check size={24} color="white" />}{' '}
+          </div>
+          Public
+        </label>
+
+        <Button onClick={handleCreatePlaylist}>Salvar</Button>
       </div>
     </div>
   );
