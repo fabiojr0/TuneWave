80aa648969b9548f468b556d6ddecd0e65ef06b4
fabiojr0 <fabiojunior321@yahoo.com.br>
2024-04-14 17:31:12 -0300

    Animations on genre renderization

--- src/components/Items/Genre.tsx
+++ src/components/Items/Genre.tsx
@@ -1,13 +1,16 @@
+import { useMemo } from 'react';
 import { capitalizeEachWord } from '../../utils/utils';
 
 function Genre({ title, count, maxCount }: { title: string; count: number; maxCount: number }) {
-  const widthPercentage = Math.min(100, (count / maxCount) * 100);
+  const widthPercentage = useMemo(() => {
+    return Math.min(100, (count / maxCount) * 100);
+  }, [count, maxCount]);
 
   return (
     <div className="flex flex-col group">
       <span className="font-semibold pl-2">{capitalizeEachWord(title)}</span>
       <span
-        className="bg-gradient-to-r to-lightGreen from-darkGreen shadow-white/50 shadow-inner rounded-e h-8 max-w-[80vw]"
+        className="bg-gradient-to-r to-lightGreen from-darkGreen shadow-white/50 shadow-inner rounded-e h-8 max-w-[80vw] transition-all"
         style={{ width: `${widthPercentage}%` }}
       ></span>
     </div>
